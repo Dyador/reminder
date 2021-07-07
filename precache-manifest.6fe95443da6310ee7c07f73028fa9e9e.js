c533718c1f938225413269563aa5823566f886a3
Drutskiy Volodymyr <dyadorunity@gmail.com>
2021-07-07 13:40:38 +0300

    Updates

---
+++ precache-manifest.6fe95443da6310ee7c07f73028fa9e9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6f608ffd4dc299233a69253769a7f3",
+    "url": "/reminder/index.html"
+  },
+  {
+    "revision": "56b42bd7ca96b75b57fa",
+    "url": "/reminder/static/css/main.f4903a8f.chunk.css"
+  },
+  {
+    "revision": "f09b246faf15a1ff0037",
+    "url": "/reminder/static/js/2.85a581b9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/reminder/static/js/2.85a581b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b42bd7ca96b75b57fa",
+    "url": "/reminder/static/js/main.7836f899.chunk.js"
+  },
+  {
+    "revision": "1dba845d14afaee7b6be",
+    "url": "/reminder/static/js/runtime-main.86971e2f.js"
+  }
+]);
